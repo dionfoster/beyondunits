cd8b47ce7a1260bd9a14e0ed5441550e550de1d0
Dion Foster <dion.foster@gmail.com>
2014-03-30 17:45:22 +0800

    - Add an image approval test.

---
+++ src/BeyondUnitsDemo.Tests/ImageTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using Approvals = ApprovalTests.Approvals;
+
+namespace BeyondUnitsDemo.Tests
+{
+    [TestFixture]
+    public class ImageTests
+    {
+        [UseReporter(typeof(ImageReporter))]
+        [Test]
+        public void Test_an_image()
+        {
+            var imagePath = Path.Combine(Environment.CurrentDirectory, "test.jpg");
+
+            if (File.Exists(imagePath))
+            {
+                File.Delete(imagePath);
+            }
+
+            var imageInfo = new FileInfo(imagePath);
+
+            ImageGenerator.Create(imagePath, "Test it!!");
+
+            Approvals.Verify(imageInfo);
+        }
+    }
+}
---
+++ src/BeyondUnitsDemo/ImageGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace BeyondUnitsDemo
+{
+    public class ImageGenerator
+    {
+        public static void Create(string filePath, string text)
+        {
+            var font = new Font("Tahoma", 24);
+
+            using (var tempImage = new Bitmap(1, 1))
+            {
+                using (var tempDrawing = Graphics.FromImage(tempImage))
+                {
+                    var textSize = tempDrawing.MeasureString(text, font);
+
+                    using (var img = new Bitmap((int)textSize.Width, (int)textSize.Height))
+                    {
+                        using (var drawing = Graphics.FromImage(img))
+                        {
+                            drawing.Clear(Color.White);
+
+                            using (var textBrush = new SolidBrush(Color.Black))
+                            {
+                                drawing.DrawString(text, font, textBrush, 0, 0);
+
+                                drawing.Save();
+                            }
+                        }
+
+                        img.Save(filePath, ImageFormat.Jpeg);
+                    }
+                }
+            }
+        }
+    }
+}
